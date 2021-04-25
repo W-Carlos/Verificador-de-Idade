3eeecfd61430ed34a6d6f5d3031d005659cb759c
W.Carlos <wendcarlos18@hotmail.com>
2021-04-25 19:57:07 -0300

    fiz a function verificar

---
+++ script.js
@@ -0,0 +1,8 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()//Vai pegar o ano com 4 digitos.
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')//ou document.querySelector('div#res')
+
+    
+}
